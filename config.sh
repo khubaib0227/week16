66b0f4016bb4bced7484df793c6c421f197dc279
Khubaib Amjad <l1f21bsse0227@ucp.edu.pk>
2025-01-23 12:01:44 +0500

    initial commit

---
+++ config.sh
@@ -0,0 +1,7 @@
+echo "going to configure repo"
+git branch -m develop
+git config --local user.name "Khubaib Amjad"
+git config --local user.email l1f21bsse0227@ucp.edu.pk
+git config --local core.editor notepad
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css .gitignore
